02642fd4d8097209ed68304cf7fd1eab0510089c
JaissonB <097319@aluno.uricer.edu.br>
2022-03-10 11:58:46 -0300

    Adição de verificação de erro nos inputs

--- script.js
+++ script.js
@@ -16,20 +16,45 @@ $('.normalize .div-input .inpt-text').blur(() => {
     var b = $('#norm-blue').val().trim();
     
     if (r !== '' && g !== '' && b !=='') {
-        $('.norm-picker').val(rgbToHex(r, g, b));
-        var normalized = normalizeRGB(r, g, b);
-        console.log(normalized)
-        $('#norm-red-response').val(normalized[0]);
-        $('#norm-green-response').val(normalized[1]);
-        $('#norm-blue-response').val(normalized[2]);
+        if (r < 0 || r > 255) {
+            $('.div-error').css('display', 'block');
+            removeNormInputErrors();
+            $('#norm-red').addClass('inpt-error');
+        } else if (g < 0 || g > 255) {
+            $('.div-error').css('display', 'block');
+            removeNormInputErrors();
+            $('#norm-green').addClass('inpt-error');
+        } else if (b < 0 || b > 255) {
+            $('.div-error').css('display', 'block');
+            removeNormInputErrors();
+            $('#norm-blue').addClass('inpt-error');
+        } else {
+            $('.div-error').css('display', 'none');
+            removeNormInputErrors();
+            $('.norm-picker').val(rgbToHex(r, g, b));
+            var normalized = normalizeRGB(r, g, b);
+            console.log(normalized)
+            $('#norm-red-response').val(normalized[0]);
+            $('#norm-green-response').val(normalized[1]);
+            $('#norm-blue-response').val(normalized[2]);
+        }
     }
 })
 
+//Remove a classe de erro de todos os inputs
+function removeNormInputErrors () {
+    $('#norm-red').removeClass('inpt-error');
+    $('#norm-green').removeClass('inpt-error');
+    $('#norm-blue').removeClass('inpt-error');
+}
+
+//Transforma cor em hexadecimal para RGB (retorna um array[r, g, b])
 function hexToRgb (hex) {
     return hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i,(m, r, g, b) => '#' + r + r + g + g + b + b)
     .substring(1).match(/.{2}/g).map(x => parseInt(x, 16));
 }
 
+//Transforma cor em RGB para hexadecimal (retorna uma string)
 function rgbToHex(r, g, b) {
     r = parseInt(r).toString(16);
     g = parseInt(g).toString(16);
@@ -47,6 +72,7 @@ function rgbToHex(r, g, b) {
     return hex;
 }
 
+//Normaliza a cor RGB (retorna um array com as cores normalizadas)
 function normalizeRGB (r, g, b) {
     r = parseInt(r);
     g = parseInt(g);
@@ -55,6 +81,6 @@ function normalizeRGB (r, g, b) {
     var red = r / (r + g + b);
     var green = g / (r + g + b);
     var blue = b / (r + g + b);
-    
+
     return [red, green, blue];
 }
